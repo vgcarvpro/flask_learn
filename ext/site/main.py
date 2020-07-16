9e71f235cae1d8add119d2a6bcdcb4e23d551e11
vgcarvpro <vgcarvpro@gmail.com>
2020-07-15 22:32:42 -0300

    pastas

---
+++ ext/site/main.py
@@ -0,0 +1,22 @@
+from flask import render_template
+from flask import Blueprint
+from flask import current_app
+
+bp = Blueprint("site", __name__)
+
+
+@bp.route("/")
+def index():
+    print("entrei na funcao main")
+    current_app.logger.debug("Entrei na funcao main")
+    return render_template("index.html")
+
+
+@bp.route("/sobre")
+def about():
+    return render_template("about.html")
+
+
+@bp.route("/testes")
+def restaurants():
+    return render_template("testes.html")
