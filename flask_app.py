a38c86c4f8859f5b2de10060694d38768b08979b
vgcarvpro <vgcarvpro@gmail.com>
2020-07-17 14:44:18 -0300

    init

--- flask_app.py
+++ flask_app.py
@@ -10,7 +10,7 @@ from ext import toolbar
 def create_app():
     app = Flask(__name__)
     config.init_app(app)
-    toolbar.init_app(app)
+    #toolbar.init_app(app)
     site.init_app(app)
     return app
 # flask no pythonanyware
